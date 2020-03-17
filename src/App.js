4365c4523424ff26dfef7d11e1524d8ca5b179ee
Vladislav <superiorite.vladislav@gmail.com>
2020-03-17 20:55:17 +0500

    Add menu

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <Menu />
+    );
+  }
+}
+
+
+class Menu extends React.Component {
+  render() {
+    let menus = [
+      'Проекты',
+      'Обо мне',
+      'Блог',
+      'Контакты'
+      ]
+    return (
+      <div className="menu">
+        {menus.map((v, i) => {
+          return <div key={i} className="ccd"><a className="ddott" href="#">{v}</a></div>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
