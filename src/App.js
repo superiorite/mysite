2202899b032dde856bb4b23ef909bd54f4289b5c
Vladislav <superiorite.vladislav@gmail.com>
2020-03-18 11:27:45 +0500

    Change menu with React Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 
 class App extends React.Component {
@@ -9,7 +14,11 @@ class App extends React.Component {
   }
   render() {
     return (
-      <Menu />
+      <div className="main">
+        <Menu />
+        <Logo />
+        <Quote />
+      </div>
     );
   }
 }
@@ -17,20 +26,74 @@ class App extends React.Component {
 
 class Menu extends React.Component {
   render() {
-    let menus = [
-      'Проекты',
-      'Обо мне',
-      'Блог',
-      'Контакты'
-      ]
     return (
       <div className="menu">
-        {menus.map((v, i) => {
-          return <div key={i} className="ccd"><a className="ddott" href="#">{v}</a></div>
-        })}
+        <Router>
+          <div>
+            <nav>
+              <div className="ccd">
+                <Link to="/projects" className="ddott">Проекты</Link>
+              </div>
+              <div className="ccd">
+                <Link to="/about" className="ddott">Обо мне</Link>
+              </div>
+              <div className="ccd">
+                <Link to="/blog" className="ddott">Блог</Link>
+              </div>
+              <div className="ccd">
+                <Link to="/contacts" className="ddott">Контакты</Link>
+              </div>
+            </nav>
+
+            <Switch>
+              <Route path="/projects">
+                <Projects />
+              </Route>
+              <Route path="/blog">
+                <Blog />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/contacts">
+                <Contacts />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
     );
   }
+}
+
+function Projects() {
+  return <h2>Проекты</h2>;
+}
+
+function About() {
+  return <h2>Обо мне</h2>;
+}
+
+function Blog() {
+  return <h2>Блог</h2>;
+}
+
+function Contacts() {
+  return <h2>Контакты</h2>;
+}
+
+const Quote = props => {
+  return (
+    <div className="quote">
+      Идеи появляются и исчезают,<br/>
+      истории остаются.
+    </div>
+  );
+}
+
+
+const Logo = props => {
+  return <img src="./superiorite.png"/>
 }
 
 export default App;
